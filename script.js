5f2a8c8f6f79aaeecdbdab1c2f66cac00731821d
Abhishek sonawane <abhisheksonwane57@gmail.com>
2022-07-16 00:05:12 +0530

    feat: added api fetching and output logic

---
+++ script.js
@@ -0,0 +1,16 @@
+const apiUrl = 'https://api.adviceslip.com/advice'
+const advice_num = document.querySelector('.advice-no')
+const advice_output = document.querySelector('.advice-head')
+const adviceBtn = document.querySelector('.new-quote')
+
+
+
+async function getAdvice(){
+    const response = await fetch(apiUrl);
+    const data = await response.json()
+    advice_num.innerText = "ADVICE #"+ data.slip.id;
+    advice_output.innerText = data.slip.advice;
+}
+
+
+adviceBtn.addEventListener('click',getAdvice)
